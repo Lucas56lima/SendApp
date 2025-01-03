749dbd180aa7b411a5121d7b4771c4eef54f0d7b
Lucas56lima <lucas56lima49@gmail.com>
2025-01-03 17:15:35 -0300

    implementando serviço de e-mail e de captura de xmls

---
+++ SendApp/Program.cs
@@ -0,0 +1,27 @@
+using Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+var mailService = new MailService();
+mailService.GetFileForPath(@"C:\\Users\\Usuário\\Desktop\\Nova pasta");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Service/MailService.cs
@@ -0,0 +1,100 @@
+﻿using System.Net.Mail;
+using System.Net;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+
+namespace Service
+{
+    public class MailService
+    {
+        public void SendMail(string email, string password, string file)
+        {
+            try
+            {
+                SmtpClient smtpClient = new("smtp.deco.com.br")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(email, password),
+                    EnableSsl = false,
+                };
+
+                MailMessage mail = new()
+                {
+                    From = new MailAddress(email),
+                    Subject = "Teste",
+                    Body = "Outro teste",
+                    IsBodyHtml = false
+                };
+
+                mail.To.Add("fiscal@deco.com.br");
+                Attachment attachment = new(file);
+                mail.Attachments.Add(attachment);
+                smtpClient.Send(mail);
+
+                Console.WriteLine("E-mail enviado com sucesso!");
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine($"Erro SMTP: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Detalhes da exceção interna: {ex.InnerException.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro geral: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Detalhes da exceção interna: {ex.InnerException.Message}");
+                }
+            }
+        }
+
+        public void GetFileForPath(string path)
+        {
+            string fileZip = @"C:\\Users\\Usuário\\Desktop\\Nova pasta\\Arquivo.zip";
+
+            try
+            {
+                var filterFiles = Directory.EnumerateFiles(path, "*.xml", SearchOption.AllDirectories)
+                    .Where(f =>
+                    {
+                        //DateTime now = DateTime.Now;
+                        DateTime fileCreation = File.GetLastWriteTime(f);
+                        DateTime now = DateTime.Now;
+                        if ((now.Month == 1 && fileCreation.Month == 12) && (now.Year > fileCreation.Year) )
+                        {
+                            return f.EndsWith(".xml");
+                        }
+                        
+                        return f.EndsWith(".xml") 
+                               && fileCreation.Month == now.AddMonths(-1).Month;
+                    });
+
+                using (var archive = ArchiveFactory.Create(ArchiveType.Zip))
+                {
+                    foreach (var file in filterFiles)
+                    {
+                        // Adicionando o arquivo diretamente ao arquivo Zip
+                        archive.AddEntry(Path.GetFileName(file), file);
+                        
+                    }
+
+                    using (var stream = File.OpenWrite(fileZip))
+                    {
+                        archive.SaveTo(stream, CompressionType.None);
+                    }
+
+                    SendMail("lucas.feitosa@deco.com.br", "15975323Lu.", fileZip);
+                }
+
+                Console.WriteLine("Arquivos .zip criados com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao ler arquivos: {ex.Message}"); // Corrigido para exibir a exceção corretamente
+            }
+        }
+    }
+}
